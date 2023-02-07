c65f6d09fcb752dbdd36f08226ffc2c24ca8739e
Mauricio Tremea Zaquia <mauriciotzaquia@gmail.com>
2023-02-07 09:20:17 +0100

    adding comments for later reference

--- NavigationReducer/Sources/NavigationReducer/NavigatableAction.swift
+++ NavigationReducer/Sources/NavigationReducer/NavigatableAction.swift
@@ -8,7 +8,8 @@ import ComposableArchitecture
 public enum NavigationAction<Destination: NavigationDestination> {
     case navigation(Destination.NavigationAction)
     case root(Destination.RootReducer.Action)
-    case destination(Destination.ID?, Destination.Action)
-
+    // TODO: With an optional ID, BindingReducers do not work within destination reducers...
+    case destination(Destination.ID, Destination.Action)
+    // ... so how to accomplish this?
 //    static func destinationModal(_ action: Destination.Action) -> Self { .destination(nil, action) }
 }
